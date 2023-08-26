25882baf747914b49ef3ff2639fca4232acc54dc
stevrasjid <79348866+stevrasjid@users.noreply.github.com>
2023-08-26 22:59:37 +0700

    pagination baru dikit

--- resources/js/Elements/Pagination/Pagination.jsx
+++ resources/js/Elements/Pagination/Pagination.jsx
@@ -1,39 +1,63 @@
 import React from "react";
 
 export default function Pagination(props) {
-  const { pageNumber, totalCount, onChange } = props;
+    const { pageNumber, totalCount, onClick } = props;
 
-  for(var i = 1;i <= totalCount;++){
-    
-  }
-  
-  return (
-    <nav aria-label="Page navigation example">
-      <ul className="pagination justify-content-center">
-        <li
-          className={`page-item ${pageNumber == 1 ? "disabled" : ""}`}
-          onChange={onChange(pageNumber - 1)}
-        >
-          <a className="page-link" href="#" tabindex="-1">
-            Previous
-          </a>
-        </li>
-        {totalCount.map((data, index) => (
-          <li className="page-item">
-            <a className="page-link" onChange={onChange(index)}>
-              {index}
-            </a>
-          </li>
-        ))}
+    const handlePageChange = (index) => {
+        if (index < 1 || index > totalCount) {
+            return;
+        }
 
-        <li
-          className={`page-item ${pageNumber == totalCount ? "disabled" : ""}`}
-        >
-          <a className="page-link" onChange={onChange(pageNumber + 1)}>
-            Next
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
+        onClick(index);
+    };
+
+    const renderPageNumber = () => {
+        // prev 1 2 3 4 5 ... next bold di 1
+        // prev 1 2 3 4 5 ... next bold di 2
+        // prev 1 2 3 4 5 ... next bold di 3
+        // prev ... 3 4 5 6 ... next bold di 4
+        // prev ... 4 5 6 7 ... next bold di 5
+        // prev ... 10 11 12 13 next bold di 11
+        // prev ... 10 11 12 13 next bold di 12
+        // prev ... 10 11 12 13 next bold di 13
+        // maunya begini
+        const pageNumbers = [];
+        for (let i = 1; i < totalCount; i++) {
+            pageNumbers.push(
+                <span
+                    key={i}
+                    className={i === currentPage ? "active" : ""}
+                    onClick={() => handlePageChange(i)}
+                ></span>
+            );
+        }
+    };
+
+    return (
+        <nav aria-label="Page navigation example">
+            <ul className="pagination justify-content-center">
+                <li
+                    className={`page-item ${pageNumber == 1 ? "disabled" : ""}`}
+                    onChange={onChange(pageNumber - 1)}
+                >
+                    <a className="page-link" href="#" tabindex="-1">
+                        Previous
+                    </a>
+                </li>
+                {renderPageNumber}
+                <li
+                    className={`page-item ${
+                        pageNumber == totalCount ? "disabled" : ""
+                    }`}
+                >
+                    <a
+                        className="page-link"
+                        onChange={onChange(pageNumber + 1)}
+                    >
+                        Next
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
